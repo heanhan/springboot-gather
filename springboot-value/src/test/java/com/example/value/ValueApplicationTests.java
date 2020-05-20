02511ed66aeb195c206e53c64f6464e1eff69b3c
zhaojh0912 <1763124707@qq.com>
2020-05-20 11:21:44 +0800

    2020-05-20:提交 内容： 1、添加使用配置 文件读取配置文件的属性模块案例，使用 racketmq 的 消息 队列模块

---
+++ springboot-value/src/test/java/com/example/value/ValueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.value;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
