35277b65d196d8ca8d010cf88fa1bd0948915c82
zhaojh0912 <1763124707@qq.com>
2020-05-28 23:34:16 +0800

    2020-05-28:提交 内容： 1、提交 springboot-cannal  的 数据 同步的模块

---
+++ springboot-cannal/src/main/java/com/example/cannal/SpringbootCannalApplication.java
@@ -0,0 +1,13 @@
+package com.example.cannal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCannalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCannalApplication.class, args);
+    }
+
+}
---
+++ springboot-cannal/src/test/java/com/example/cannal/SpringbootCannalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cannal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCannalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
