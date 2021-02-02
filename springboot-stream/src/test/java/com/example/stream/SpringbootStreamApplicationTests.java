9e8d4cf4ceb1b68f1618eb1cc89464bd342c3f57
zhaojh0912 <1763124707@qq.com>
2021-02-02 14:59:40 +0800

    2021-02-02:提交 内容： 1、提交使用 springcloud stream 的 消息 驱动的应用 moudle

---
+++ springboot-stream/src/test/java/com/example/stream/SpringbootStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
