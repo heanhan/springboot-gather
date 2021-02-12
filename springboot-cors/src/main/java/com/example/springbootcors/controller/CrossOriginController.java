9d7166e492a655eb8fb6c0e59f1ef2ca9a3c8b97
zhaojh0912 <1763124707@qq.com>
2021-02-12 16:09:26 +0800

    2021-02-12:提交 内容： 1、springboot-cron 跨域的  解决方案模块的代码提交

---
+++ springboot-cors/src/main/java/com/example/springbootcors/controller/CrossOriginController.java
@@ -0,0 +1,16 @@
+package com.example.springbootcors.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author :zhaojh0912
+ * @Date : 2021/2/11 2:41 下午
+ * @Version : 1.0
+ * @Description :T测试使用跨域的继绝方案
+ **/
+
+@Slf4j
+@RestController
+public class CrossOriginController {
+}
---
+++ springboot-cors/src/test/java/com/example/springbootcors/SpringbootCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
